4590419713bf9f702fd672f69792b342b3b4bbee
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2016-05-30 14:57:00 +0200

    First Load

---
+++ JavaWorldGitHub/src/helloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * PMT/PT1 Praktikum Aufgabenblatt 
+ * @author Johannes Kruber
+ * @author Michel Gerlach 
+ */
+package helloWorld;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World: Für GitHub");
+	}
+
+}
